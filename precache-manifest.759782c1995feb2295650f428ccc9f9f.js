44046ccf21b886982da429b6e10a82dc5adbdf9c
Oleksandr <sashaka2013@gmail.com>
2020-03-28 17:38:58 +0200

    Updates

---
+++ precache-manifest.759782c1995feb2295650f428ccc9f9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91eb32fb0c9de38773686dcbc3b5ec46",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "217c827f8c26b45e1cc7",
+    "url": "/weather-app/static/css/main.37b861e4.chunk.css"
+  },
+  {
+    "revision": "181985f81263d36bff2a",
+    "url": "/weather-app/static/js/2.1222dca0.chunk.js"
+  },
+  {
+    "revision": "031b62b957b4fb917cd0ec81e2c8bd7b",
+    "url": "/weather-app/static/js/2.1222dca0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "217c827f8c26b45e1cc7",
+    "url": "/weather-app/static/js/main.6da54434.chunk.js"
+  },
+  {
+    "revision": "55676f7ecad5f9b6942e",
+    "url": "/weather-app/static/js/runtime-main.58bc7688.js"
+  }
+]);
